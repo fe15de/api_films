3e700290ecc9389cfc9c0807c51ea446d285e0a3
Federico <federicoperezramirez@gmail.com>
2025-09-09 23:02:35 -0500

    map of films, getting started on searching functions of films

--- api.py
+++ api.py
@@ -7,9 +7,28 @@ response = requests.get(URL)
 soup = BeautifulSoup(response.text, "html.parser")
 
 #GET ONLY THE CONTAINERS OF FILMS
-all = soup.select(".movie-item")
+all_films = soup.select(".movie-item")
+films_map= {}
 
-for film in all:
+for film in all_films:
+    
     #GET ORIGINAL NAME OF FILM 
-    print(film.select_one('.movie-item__title').get_text(strip=True))
+    name = film.select_one('.movie-item__title').get_text(strip=True)
+    
+    #fix name so that can the function time can be searched by name
+    name_to_search = name.replace(' ','-')
+    name_to_search = name_to_search.replace(':','-')
+    name_to_search = name_to_search.replace('--','-')
 
+    films_map[name] = [name_to_search]
+
+search_functions =films_map['Un Poeta']
+
+search_time_functions = f'https://www.cinecolombia.com/bogota/peliculas/{search_functions}'
+response = requests.get(URL)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+#GET ONLY THE CONTAINERS OF FILMS
+idk = soup.select(".show-times-collapse__header")
+print(idk)
